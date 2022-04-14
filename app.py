3e983e2da184750c3ac0ecf5420bfb9b07739060
vibodhbhosure <vibodhbhoure@gmail.com>
2022-04-14 11:14:23 +0530

    nothing

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from datetime import datetime
-import numpy as np
-import pandas as pd
+import numpy
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 from keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
-
-start = '2010-01-01'
-end = datetime.today().strftime("%Y-%m-%d")
+import yfinance as yf
 
 st.title('Stock Trend Prediction')
 
 user_input = st.text_input("Enter Stock Ticker", "AAPL")
+
+#start = '2010-01-01'
+start = str(yf.Ticker(user_input).history(period='max').reset_index()['Date'][0])[:10]
+#end = '2014-02-16'
+end = datetime.today().strftime("%Y-%m-%d")
+
 df = data.DataReader(user_input, 'yahoo', start, end)
+df = df.reset_index()
 
 st.subheader(str(start)+" To "+str(end))
 st.write(df.describe())
@@ -39,43 +43,109 @@ plt.plot(ma200, 'g')
 plt.plot(df.Close)
 st.pyplot(fig)
 
+df1 = df.reset_index()['Close']
+
 #Split Data into Training and Testing
-data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
-
 scaler = MinMaxScaler(feature_range=(0, 1))
-
-data_training_array = scaler.fit_transform(data_training)
+df1 = scaler.fit_transform(numpy.array(df1).reshape(-1, 1))
+train_data, test_data = df1[0:int(len(df1)*0.70), :], df1[int(len(df1)*0.70):len(df1), :1]
 
 #Load my model
-model = load_model('keras_model.h5')
+model = load_model('new_model.h5')
 
-#Testing Part
-past_100 = data_training.tail(100)
-final_df = past_100.append(data_testing, ignore_index=True)
-input_data = scaler.fit_transform(final_df)
-x_test = []
-y_test = []
-for i in range(100, input_data.shape[0]):
-    x_test.append(input_data[i - 100:i])
-    y_test.append(input_data[i, 0])
 
-x_test, y_test = np.array(x_test), np.array(y_test)
-y_predicted = model.predict(x_test)
-scaler = scaler.scale_
+def create_dataset(dataset, time_step=1):
+    dataX,dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a=dataset[i:(i+time_step), 0]
+        dataX.append(a)
+        dataY.append(dataset[i+time_step,0])
+    return numpy.array(dataX), numpy.array(dataY)
 
-scale_factor = 1/scaler[0]
-y_predicted = y_predicted * scale_factor
-y_test = y_test * scale_factor
+time_step=100
+x_train, y_train = create_dataset(train_data, time_step)
+x_test, y_test = create_dataset(test_data, time_step)
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+train_predict = model.predict(x_train)
+test_predict = model.predict(x_test)
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
 
-#Final Graph
-st.subheader('Predictions vs Original')
-fig2 = plt.figure(figsize=(12, 6))
-plt.plot(y_test, 'b', label='Original Price')
-plt.plot(y_predicted, 'r', label='Predicted Price')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend()
+loopback = 100
+st.subheader('Testing of Prediction')
+fig6 = plt.figure(figsize=(12, 6))
+trainPredictPlot = numpy.empty_like(df1)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[loopback:len(train_predict)+loopback, :] = train_predict
+testPredictPlot = numpy.empty_like(df1)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(train_predict)+(loopback*2)+1:len(df1)-1, :] = test_predict
+plt.plot(scaler.inverse_transform(df1))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
 plt.show()
-st.pyplot(fig2)
+st.pyplot(fig6)
+
+
+x_input=test_data[len(test_data)-100:].reshape(1,-1)
+temp_input=list(x_input)
+temp_input=temp_input[0].tolist()
+
+
+lst_output = []
+n_steps = 100
+i = 0
+npt = int(st.text_input("How many days of data to predict?", "10"))
+while (i < npt):
+
+    if (len(temp_input) > 100):
+        # print(temp_input)
+        x_input = numpy.array(temp_input[1:])
+        print("{} day input {}".format(i, x_input))
+        x_input = x_input.reshape(1, -1)
+        x_input = x_input.reshape((1, n_steps, 1))
+        # print(x_input)
+        yhat = model.predict(x_input, verbose=0)
+        print("{} day output {}".format(i, yhat))
+        temp_input.extend(yhat[0].tolist())
+        temp_input = temp_input[1:]
+        # print(temp_input)
+        lst_output.extend(yhat.tolist())
+        i = i + 1
+    else:
+        x_input = x_input.reshape((1, n_steps, 1))
+        yhat = model.predict(x_input, verbose=0)
+        print(yhat[0])
+        temp_input.extend(yhat[0].tolist())
+        print(len(temp_input))
+        lst_output.extend(yhat.tolist())
+        i = i + 1
+
+day_new = numpy.arange(1,101)
+day_pred = numpy.arange(101,101+npt)
+
+st.subheader('Limited Prediction')
+fig7 = plt.figure(figsize=(12, 6))
+df3 = df1.tolist()
+df3.extend(lst_output)
+plt.plot(day_new, scaler.inverse_transform(df1[len(df1)-100:]),'g')
+plt.plot(day_pred, scaler.inverse_transform(lst_output),'r')
+df3 = df1.tolist()
+df3.extend(lst_output)
+plt.plot(df3[12000:])
+st.pyplot(fig7)
+
+
+st.subheader('Overall Prediction')
+fig8 = plt.figure(figsize=(12, 6))
+df3 = df1.tolist()
+df3.extend(lst_output)
+#plt.plot(day_new,scaler.inverse_transform(df1[len(df1)-100:]))
+#plt.plot(day_pred,scaler.inverse_transform(lst_output))
+df3 = df1.tolist()
+df3.extend(lst_output)
+plt.plot(df3[:],'g')
+st.pyplot(fig8)
+
 
